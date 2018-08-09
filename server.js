2383297e13fedd9c2155731cdc3e44664fb9dab7
Vivek <vivek.1886@gmail.com>
2018-08-10 00:14:24 +0530

    Set up Heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -22,9 +23,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.render('maintainence');
-});
+});*/
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
@@ -43,6 +44,6 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
